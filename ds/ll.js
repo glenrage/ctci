8ccd679e1e70637b988ad7d64afeaa26f6907f8d
galang pham <phamgalang@gmail.com>
2017-10-15 16:04:45 -0700

    added DS js structures

---
+++ ds/ll.js
@@ -0,0 +1,17 @@
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(value, next, prev) {
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
+}
+
+LinkedList.prototype.addToHead = function(value) {
+  var newNode = new Node(value, this.head, null);
+};
+var LL = new LinkedList();
+var node1 = new Node(10, 'node2', null);
+console.log(node1);
