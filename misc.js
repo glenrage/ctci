c1a8df716ca4f2ea136724f0a428ffc1a6f7cac0
galang pham <phamgalang@gmail.com>
2017-10-17 14:23:03 -0700

    added substring finder func

--- misc.js
+++ misc.js
@@ -154,3 +154,22 @@ function sumFibs(num) {
   });
   return res;
 }
+
+function subStringFinder(str, subStr) {
+  var idx = 0,
+    i = 0,
+    j = 0,
+    len = str.length,
+    subLen = subStr.length;
+
+  for (; i < len; i++) {
+    if (str[i] == subStr[j]) j++;
+    else j = 0;
+
+    //check starting point or a match
+    if (j == 0) idx = i;
+    else if (j == subLen) return idx;
+  }
+
+  return -1;
+}
